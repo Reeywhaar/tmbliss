4f03e75fb9a71edb6af42702141d1552e7ad32cc
Vyrtsev Mikhail <reeywhaar@gmail.com>
2023-09-09 23:38:32 +0300

    Add test for recursive_directory_iterator

--- src/recursive_directory_iterator.rs
+++ src/recursive_directory_iterator.rs
@@ -36,3 +36,43 @@ impl<'a> RecursiveDirectoryIterator<'a> {
         path.canonicalize().unwrap_or(path)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{cell::RefCell, rc::Rc};
+
+    use uuid::Uuid;
+
+    use crate::test_utils::{join_path, path_to_string, unzip, TestDir};
+
+    use super::*;
+
+    #[test]
+    fn it_works_recursively() {
+        let cwd = &path_to_string(&fs::canonicalize("./").unwrap());
+
+        let zip = join_path(cwd, "test_assets/test_dir.zip");
+        let unzipdir = TestDir {
+            path: join_path(cwd, &format!("test_assets_{}", Uuid::new_v4())),
+        };
+        let dir = join_path(&unzipdir.path, "test_dir/test_repo");
+
+        unzip(&zip, &unzipdir.path);
+
+        let paths = Rc::new(RefCell::new(Vec::<String>::new()));
+
+        let iterator = RecursiveDirectoryIterator {
+            path: dir,
+            op: &|path| {
+                let paths = paths.clone();
+                let mut paths = paths.try_borrow_mut().unwrap();
+                paths.push(path.to_string());
+                Ok(())
+            },
+        };
+
+        iterator.iterate().unwrap();
+
+        assert_eq!(paths.clone().try_borrow().unwrap().len(), 38);
+    }
+}
